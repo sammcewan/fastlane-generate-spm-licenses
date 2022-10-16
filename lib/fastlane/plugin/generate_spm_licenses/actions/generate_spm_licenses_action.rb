72c9e0a2a6e49e3bd37f806f4d646187919a3753
Sam McEwan <sam@mcewan.nz>
2022-10-17 08:14:39 +1300

    Adding ability to exclude

--- lib/fastlane/plugin/generate_spm_licenses/actions/generate_spm_licenses_action.rb
+++ lib/fastlane/plugin/generate_spm_licenses/actions/generate_spm_licenses_action.rb
@@ -1,5 +1,5 @@
-require 'fastlane/action'
-require_relative '../helper/generate_spm_licenses_helper'
+require "fastlane/action"
+require_relative "../helper/generate_spm_licenses_helper"
 
 module Fastlane
   module Actions
@@ -7,49 +7,54 @@ module Fastlane
 
     class GenerateSpmLicensesAction < Action
       def self.run(params)
-        require 'plist'
-        require 'json'
-        require 'uri'
+        require "plist"
+        require "json"
+        require "uri"
 
         begin
           expanded_path = File.expand_path(params[:workspace])
-          workspace_path = File.join(expanded_path, 'xcshareddata', 'swiftpm', 'Package.resolved')
+          workspace_path = File.join(expanded_path, "xcshareddata", "swiftpm", "Package.resolved")
           file = File.read(workspace_path)
           json = JSON.parse(file)
 
-          json['pins']
+          json["pins"]
 
           derived_data_path = File.expand_path(params[:derived_data_path])
           workspace_name = File.basename(expanded_path, File.extname(expanded_path))
           workspace_derived_data_folder = Dir["#{derived_data_path}/#{workspace_name}**"].first
-          checkout_folder = File.join(workspace_derived_data_folder, 'SourcePackages', 'checkouts', '/**')
+          checkout_folder = File.join(workspace_derived_data_folder, "SourcePackages", "checkouts", "/**")
           checkout_folders = Dir[checkout_folder]
 
           if checkout_folders.empty?
             UI.user_error!("No checked out folders at '#{checkout_folder}'")
           end
 
+          excluded = params[:exclude] || []
+
           # Grab licenses
           licenses = checkout_folders
-                     .filter_map do |path|
-                       name = URI(path).path.split('/').last
+            .filter_map do |path|
+            name = URI(path).path.split("/").last
 
-                       UI.message("Found license for #{name}")
-                       raw_license = File.join(path, 'LICENSE')
-                       md_license = File.join(path, 'LICENSE.md')
-                       txt_license = File.join(path, 'LICENSE.txt')
-                       if File.exist?(raw_license)
-                         License.new(name, File.read(raw_license))
-                       elsif File.exist?(md_license)
-                         License.new(name, File.read(md_license))
-                       elsif File.exist?(txt_license)
-                         License.new(name, File.read(txt_license))
-                       end
-                     end
-                     .filter_map do |license|
-            { 'Title' => license.name, 'Type' => 'PSGroupSpecifier', 'FooterText' => license.text }
+            raw_license = File.join(path, "LICENSE")
+            md_license = File.join(path, "LICENSE.md")
+            txt_license = File.join(path, "LICENSE.txt")
+            if excluded.include?(name)
+              UI.message("Skipping license for #{name}")
+              nil
+            elsif File.exist?(raw_license)
+              License.new(name, File.read(raw_license))
+            elsif File.exist?(md_license)
+              License.new(name, File.read(md_license))
+            elsif File.exist?(txt_license)
+              License.new(name, File.read(txt_license))
+            end
           end
-          wrapped_settings = { 'StringsTable' => 'Acknowledgements', 'PreferenceSpecifiers' => licenses }
+            .filter_map do |license|
+            UI.message("Found license for #{license.name}")
+            { "Title" => license.name, "Type" => "PSGroupSpecifier", "FooterText" => license.text }
+          end
+          wrapped_settings = { "StringsTable" => "Acknowledgements", "PreferenceSpecifiers" => licenses }
           destination_path = File.expand_path(params[:destination])
           File.write(destination_path, wrapped_settings.to_plist)
         rescue StandardError => e
@@ -59,7 +64,7 @@ module Fastlane
 
       # Helper Methods
       def self.xcode_preferences
-        file = File.expand_path('~/Library/Preferences/com.apple.dt.Xcode.plist')
+        file = File.expand_path("~/Library/Preferences/com.apple.dt.Xcode.plist")
         if File.exist?(file)
           plist = CFPropertyList::List.new(file: file).value
           return CFPropertyList.native_types(plist) unless plist.nil?
@@ -72,37 +77,42 @@ module Fastlane
       #####################################################
 
       def self.description
-        'A short description with <= 80 characters of what this action does'
+        "A short description with <= 80 characters of what this action does"
       end
 
       def self.details
-        'You can use this action to do cool things...'
+        "You can use this action to do cool things..."
       end
 
       def self.available_options
-        path = xcode_preferences ? xcode_preferences['IDECustomDerivedDataLocation'] : nil
-        path ||= '~/Library/Developer/Xcode/DerivedData'
+        path = xcode_preferences ? xcode_preferences["IDECustomDerivedDataLocation"] : nil
+        path ||= "~/Library/Developer/Xcode/DerivedData"
         [
           FastlaneCore::ConfigItem.new(key: :workspace,
-                                       env_name: 'FL_LICENSES_WORKSPACE_PATH',
-                                       description: 'Path for workspace',
+                                       env_name: "FL_LICENSES_WORKSPACE_PATH",
+                                       description: "Path for workspace",
                                        is_string: true,
                                        default_value: false),
           FastlaneCore::ConfigItem.new(key: :destination,
-                                       env_name: 'FL_LICENSES_OUTPUT_PATH',
-                                       description: 'Create a path to output to',
+                                       env_name: "FL_LICENSES_OUTPUT_PATH",
+                                       description: "Create a path to output to",
                                        is_string: true,
                                        default_value: false),
+          FastlaneCore::ConfigItem.new(key: :exclude,
+                                       env_name: "FL_LICENSES_EXCLUDE",
+                                       description: "Exclude an array or package names",
+                                       is_string: false,
+                                       default_value: false),
           FastlaneCore::ConfigItem.new(key: :derived_data_path,
-                                       env_name: 'FL_LICENSES_DERIVED_DATA_PATH',
-                                       description: 'Custom path for derivedData',
+                                       env_name: "FL_LICENSES_DERIVED_DATA_PATH",
+                                       description: "Custom path for derivedData",
                                        default_value_dynamic: true,
-                                       default_value: path)
+                                       default_value: path),
         ]
       end
 
       def self.authors
-        ['sammcewan']
+        ["sammcewan"]
       end
 
       def self.is_supported?(platform)
